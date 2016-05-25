96acd046bcde35821ca5f9c53fe21331486dad43
lolivier04 <lsecrease@gmail.com>
2016-05-24 21:53:15 -0500

    Contact Form backend

--- mail.php
+++ mail.php
@@ -4,7 +4,7 @@ $email = $_POST['email'];
 $dropdown = $POST['dropdown'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "lsecrease@gmail.com";
+$recipient = "jim@smw.tv";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
